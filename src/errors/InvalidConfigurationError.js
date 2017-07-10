82aee355b5cf183465be456416dc9469dd82b39d
Greg Linklater <greglink49@gmail.com>
2017-07-10 09:40:33 +0200

    feat(InvalidConfigurationError)

---
+++ src/errors/InvalidConfigurationError.js
@@ -0,0 +1,14 @@
+'use strict'
+
+/**
+ * InvalidConfigurationError
+ * @ignore
+ */
+class InvalidConfigurationError extends Error {
+}
+
+/**
+ * Exports
+ * @ignore
+ */
+module.exports = InvalidConfigurationError
