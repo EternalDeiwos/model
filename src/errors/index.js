3c129fca80ccd8b691aca59d7e3e047350d3e84b
Greg Linklater <greglink49@gmail.com>
2017-07-10 09:41:09 +0200

    feat(errors): export errors

--- src/errors/index.js
+++ src/errors/index.js
@@ -4,10 +4,18 @@
  * Module Dependencies
  * @ignore
  */
+const OperationError = require('./OperationError')
+const InvalidConfigurationError = require('./InvalidConfigurationError')
+const ValidationError = require('./ValidationError')
+const InternalError = require('./InternalError')
 
 /**
  * Exports
  * @ignore
  */
 module.exports = {
+  OperationError,
+  InvalidConfigurationError,
+  ValidationError,
+  InternalError,
 }
